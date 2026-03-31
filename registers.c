bccdadd1bb986137b6efd04b81b554c737f28e51
ira <rybakov.i.a.2002@gmail.com>
2026-03-31 17:02:32 +0300

    init

---
+++ registers.c
@@ -0,0 +1,41 @@
+struct registers {
+	struct {
+		union {
+			struct {
+				unsigned char f;
+				unsigned char a;
+			};
+			unsigned short af;
+		};
+	};
+	struct {
+		union {
+			struct {
+				unsigned char b;
+				unsigned char c;
+			};
+			unsigned short bc;
+		};
+	};
+	struct {
+		union {
+			struct {
+				unsigned char d;
+				unsigned char e;
+			};
+			unsigned short de;
+		};
+	};
+	struct {
+		union {
+			struct {
+				unsigned char h;
+				unsigned char l;
+			};
+			unsigned short hl;
+		};
+	};
+	unsigned char flags;
+	unsigned short sp;
+	unsigned short pc;
+} extern registers;
